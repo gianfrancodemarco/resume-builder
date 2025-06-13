4ac655f96e92d984ec21f0bded0e29d17457a218
Gianfranco Demarco <gianfrademarco@gmail.com>
2025-06-13 22:38:31 +0200

    feat: fix router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LandingPage from './components/LandingPage.vue'
 import EditorPage from './components/EditorPage.vue'
 
