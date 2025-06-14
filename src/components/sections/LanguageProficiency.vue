8175b9b6b966343c8c89449d73a087178b07455a
Gianfranco Demarco <gianfrademarco@gmail.com>
2025-06-14 13:42:41 +0200

    feat: custom components for sections

---
+++ src/components/sections/LanguageProficiency.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="language-proficiency" v-for="(item, itemIndex) in items" :key="itemIndex">
+        <div class="language-proficiency-label">{{ item.name }}</div>
+        <div class="language-proficiency-bar">
+            <div class="language-proficiency-bar-fill" :style="{ width: item.proficiency + '%' }"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
---
+++ src/components/sections/SkillsList.vue
@@ -0,0 +1,18 @@
+<template>
+    <p class="skills-list">
+        <span v-for="(item, itemIndex) in items" :key="itemIndex">
+            {{ item }}<br />
+        </span>
+    </p>
+</template>
+
+<script>
+export default {
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
