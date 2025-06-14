7b197cb021d3ba55121a692c8e741e9bb1bf1811
Gianfranco Demarco <gianfrademarco@gmail.com>
2025-06-14 17:41:52 +0200

    chore: track pageview on sa

--- src/router.js
+++ src/router.js
@@ -3,13 +3,20 @@ import LandingPage from './views/LandingPage.vue'
 import EditorPage from './views/EditorPage.vue'
 
 const routes = [
-    { path: '/', component: LandingPage },
-    { path: '/editor', component: EditorPage }
+    { path: '/', component: LandingPage, name: 'landing' },
+    { path: '/editor', component: EditorPage, name: 'editor' }
 ]
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes
+})
+
+// Track route changes with Simple Analytics
+router.afterEach((to) => {
+    if (window.sa_pageview) {
+        window.sa_pageview(to.name || to.path)
+    }
 })
 
 export default router
